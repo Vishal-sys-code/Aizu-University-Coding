a183c3c4eaa772b51331015ac531df00e9a1ee4a
Vishal Pandey <bishalpandey2001@gmail.com>
2023-12-29 15:33:18 +0530

    Create ITP1_3_A.cpp

---
+++ Introduction to Programming I/C++/ITP1_3_A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define lld long long double
+
+void FAST_IO() {
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    FAST_IO();
+    for(int i=1;i<=1000;i++){
+        cout << "Hello World" << endl;
+    }
+    return 0;
+}
