32c742cdb9d14f3b02ae5a2513c01bc83d384ae0
Vishal Pandey <bishalpandey2001@gmail.com>
2024-01-05 16:01:07 +0530

    Added Java

---
+++ Introduction to Programming I/Java/ITP1_1_A.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ITP1_1_A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World");
+        sc.close();
+    }
+}
---
+++ Introduction to Programming I/Java/ITP1_2_B.java
@@ -0,0 +1,8 @@
+package Introduction to Programming I.Java;
+import java.util.*;
+
+public class ITP1_2_B {
+    public static void main(String[] args) {
+        
+    }
+}
