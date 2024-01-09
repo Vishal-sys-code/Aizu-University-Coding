ba0a7ae6a3610e5f6084ff6dc767f546d3535bad
Vishal Pandey <bishalpandey2001@gmail.com>
2024-01-09 10:06:30 +0530

    Create ITP1_3_A.java

---
+++ Introduction to Programming I/Java/ITP1_3_A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ITP1_3_A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int cnt = 0;
+        for(int i=1; i<= 1000; i++){
+            // cnt++;
+            // System.out.println("Hello World" + " " + "Count: " + cnt);
+            System.out.println("Hello World");
+        }
+        sc.close();
+    }
+}
