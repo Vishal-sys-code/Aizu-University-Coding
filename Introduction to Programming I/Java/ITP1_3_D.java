39a6a5c05b1facb68e93551955a9e110bfcf5b94
Vishal Pandey <bishalpandey2001@gmail.com>
2024-01-17 15:20:32 +0530

    Update ITP1_3_D.java

--- Introduction to Programming I/Java/ITP1_3_D.java
+++ Introduction to Programming I/Java/ITP1_3_D.java
@@ -2,6 +2,16 @@ import java.util.*;
 public class ITP1_3_D{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int cnt = 0;
+        for (int i=a;i<b;i++){
+            if(c%i == 0){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
         sc.close();
     }
 }
