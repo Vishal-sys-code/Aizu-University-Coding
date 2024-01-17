7e5560032c8a724b20e58707341092ac09cc69e9
Vishal Pandey <bishalpandey2001@gmail.com>
2024-01-17 14:43:20 +0530

    Update ITP1_2_C.cpp
    Algorithm Updated

--- Introduction to Programming I/C++/ITP1_2_C.cpp
+++ Introduction to Programming I/C++/ITP1_2_C.cpp
@@ -10,6 +10,15 @@ void FAST_IO() {
     cout.tie(0);
 }
 
+/*
+Algorithm():
+1. Finding the sum of all the three numbers [sum = A+B+C]
+2. Finding the smallest number in all of the three numbers [Smallest = min({a,b,c})]
+3. Finding the largest number in all of the three numbers  [Largest = max({a,b,c})]
+4. Now we have all the numbers except the middle so for finding the middle number => Middle_Element = abs[sum-largest-smallest].
+5. Print all the three numbers in an asec order [Smallest, Middle, Largest]
+*/
+
 int main() {
     FAST_IO();
     ll a,b,c; cin>>a>>b>>c;
