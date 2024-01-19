aa06b169651dd1c0c1cb0aa5d48bf0856437b028
Vishal Pandey <bishalpandey2001@gmail.com>
2024-01-19 12:55:26 +0530

    Updated the setting.json and ITP1_4_B
    adding files.association in setting.json and also adding the solution of the ITP1_4_B in python. :)

--- Introduction to Programming I/Python/ITP1_4_B.py
+++ Introduction to Programming I/Python/ITP1_4_B.py
@@ -1,1 +1,9 @@
-r = int(input())
+import math
+r = float(input())
+
+area = math.pi*r*r
+circumference = 2*math.pi*r
+
+# print(f"{area:.6f} {circumference:.6f}")
+# print("------------")
+print("{:.6f} {:.6f}". format(area, circumference))
