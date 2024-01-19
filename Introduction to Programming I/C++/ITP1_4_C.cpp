b47967d409cbe29e687991318fc6a0c69cf60a8f
Vishal Pandey <bishalpandey2001@gmail.com>
2024-01-19 13:16:29 +0530

    Update ITP1_4_C.cpp

---
+++ Introduction to Programming I/C++/ITP1_4_C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+const double PI = 3.14159265359;
+
+void FAST_IO() {
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    FAST_IO();
+    
+    return 0;
+}
