866897a5a81bda7d51ac457ae5dfacde27530c4f
Vishal Pandey <bishalpandey2001@gmail.com>
2024-01-19 15:25:07 +0530

    Create ITP1_4_C.java

---
+++ Introduction to Programming I/Java/ITP1_4_C.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ITP1_4_C {
+    public static int calculate(int a, int b, char op){
+        switch(op){
+            case '+':
+                return a + b;
+            case '-':
+                return a-b;
+            case '*':
+                return a*b;
+            case '/':
+                return a/b;
+            default:
+                return -1;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
